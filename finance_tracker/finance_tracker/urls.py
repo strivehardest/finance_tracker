4877f17913b78a765de1a6c7962d4459d3364d20
Waliu Ibrahimah Aforlabi <ibrahimah2011@hotmail.com>
2025-08-30 17:32:02 +0000

    Add files via upload

--- finance_tracker/finance_tracker/urls.py
+++ finance_tracker/finance_tracker/urls.py
@@ -15,26 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from api.views import RegisterView, LoginView, LogoutView, AccountViewSet, TransactionViewSet, CategoryViewSet
-
-router = DefaultRouter()
-router.register(r'accounts', AccountViewSet, basename='accounts')
-router.register(r'transactions', TransactionViewSet, basename='transactions')
-router.register(r'categories', CategoryViewSet, basename='categories')
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/register/', RegisterView.as_view(), name='register'),
-    path('api/login/', LoginView.as_view(), name='login'),
-    path('api/logout/', LogoutView.as_view(), name='logout'),
-    path('api/', include(router.urls)),
+    path('', include('accounts.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
