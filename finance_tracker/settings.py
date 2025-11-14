1225bff387ec9c1b6a9cf409d39a61c16d2c53e7
Waliu Ibrahimah Aforlabi <ibrahimah2011@hotmail.com>
2025-11-14 14:04:11 +0000

    Add SECURE_PROXY_SSL_HEADER for SSL support

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-chang
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost, finance-tracker-muiu.onrender.com').split(',')
 
 # Application definition
 INSTALLED_APPS = [
@@ -192,4 +192,5 @@ if not DEBUG:
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
+
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
