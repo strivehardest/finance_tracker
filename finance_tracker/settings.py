640da37aa0d43c82e4457245c1089244bffcc36a
Waliu Ibrahimah Aforlabi <ibrahimah2011@hotmail.com>
2025-11-18 16:22:54 +0000

    Update settings.py

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -76,6 +76,7 @@ if config('DATABASE_URL', default=None):
         'default': dj_database_url.config(
             default=config('DATABASE_URL'),
             conn_max_age=600
+            conn_health_checks=True,
         )
     }
 else:
@@ -194,3 +195,4 @@ if not DEBUG:
     SECURE_HSTS_PRELOAD = True
 
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
