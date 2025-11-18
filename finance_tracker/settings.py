4323d97ec3a6b2db66c421238a39c389ee9b3494
Waliu Ibrahimah Aforlabi <ibrahimah2011@hotmail.com>
2025-11-18 21:03:11 +0000

    Update settings.py

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -183,11 +183,11 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST = 'smtp.sendgrid.net'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='trackerfinanace@gmail.com')
-    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+    EMAIL_HOST_USER = 'apikey'  # This is literally the word 'apikey'
+    EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY', default='')
     DEFAULT_FROM_EMAIL = 'Finance Tracker <trackerfinanace@gmail.com>'
     SERVER_EMAIL = 'trackerfinanace@gmail.com'
 
@@ -223,3 +223,4 @@ if not DEBUG:
     # Limit file upload size
     DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880  # 5MB
     FILE_UPLOAD_MAX_MEMORY_SIZE = 5242880  # 5MB
+
